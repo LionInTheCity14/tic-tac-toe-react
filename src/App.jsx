ea8c37d9202f61cb0027979847c4ba4e0b199c8c
Varun Pal Singh <singhvarun928@gmail.com>
2023-12-14 23:08:18 +0530

    react hooks and props

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Board from "./components/Board"
+
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Tic-Tac-Toe</h2>
+      <Board />
+    </>
+  )
+}
+
+export default App
